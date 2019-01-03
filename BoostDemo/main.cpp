b1d948763d620f22c1da4d5eb8f629b826934c0b
LinJK <linjk@zhenglaikeji.com>
2019-01-03 19:12:18 +0800

    测试progress_time

--- BoostDemo/main.cpp
+++ BoostDemo/main.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
+#include <sstream>
 #include <boost/version.hpp>
 #include <boost/config.hpp>
 //
+#include <boost\progress.hpp>
 #include "DateTimeUtil.h"
+
+using namespace boost;
 
 using namespace std;
 
 int 
 main(int argc, char* argv[], char* env[]) {
-	cout << "Current boost version: " << BOOST_VERSION << endl;
-	cout << "Compiler: " << BOOST_COMPILER << endl;
-	cout << "---------------------------------------" << endl;
-	// 日期时间测试
-	DateTimeUtil datetimeUtil;
-	cout << "最大计量时间(小时): " << datetimeUtil.max_compute_time() << endl;
-	cout << "最小计量时间(秒): " << datetimeUtil.min_compute_time() << endl;
-	//
+	std::stringstream ss;
+	{
+		progress_timer pt(ss);
+
+		cout << "Current boost version: " << BOOST_VERSION << endl;
+		cout << "Compiler: " << BOOST_COMPILER << endl;
+		cout << "---------------------------------------" << endl;
+		// 日期时间测试
+		DateTimeUtil datetimeUtil;
+		cout << "最大计量时间(小时): " << datetimeUtil.max_compute_time() << endl;
+		cout << "最小计量时间(秒): " << datetimeUtil.min_compute_time() << endl;
+		//
+	}
+	cout << "程序执行耗时(s): " << ss.str() << endl;
 	return 0;
 }
